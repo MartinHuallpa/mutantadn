bfcc5d9c5efbde62db9a0efedd07f94ee8d09e4c
Martin Huallpa <hualpamartin1@gmail.com>
2025-11-20 18:23:18 -0300

    DnaRecord y Repository añadidos

---
+++ src/main/java/com/utn/adn/mutantadn/repository/DnaRecordRepository.java
@@ -0,0 +1,14 @@
+package com.utn.adn.mutantadn.repository;
+
+import com.utn.adn.mutantadn.entity.DnaRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface DnaRecordRepository extends JpaRepository<DnaRecord, Long> {
+
+    Optional<DnaRecord> findByDnaHash(String dnaHash);
+
+    long countByIsMutant(boolean isMutant);
+}

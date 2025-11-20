6723f999d8269f788f07d6a917fdd2dc40885d2d
Martin Huallpa <hualpamartin1@gmail.com>
2025-11-20 16:54:06 -0300

    mutantadn

---
+++ src/main/java/com/utn/adn/mutantadn/MutantAdnApplication.java
@@ -0,0 +1,13 @@
+package com.utn.adn.mutantadn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantAdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantAdnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/adn/mutantadn/MutantAdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.adn.mutantadn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantAdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

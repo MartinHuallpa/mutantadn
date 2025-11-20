601668597649e11d7eb1e63298115e38339cf3e6
Martin Huallpa <hualpamartin1@gmail.com>
2025-11-20 17:50:36 -0300

    vld

--- src/main/java/com/utn/adn/mutantadn/validation/ValidDnaSequenceValidator.java
+++ src/main/java/com/utn/adn/mutantadn/validation/ValidDnaSequenceValidator.java
@@ -10,15 +10,15 @@ public class ValidDnaSequenceValidator implements ConstraintValidator<ValidDnaSe
 
     @Override
     public boolean isValid(String[] dna, ConstraintValidatorContext context) {
-        if (dna == null) return false; // Nulo
+        if (dna == null) return false; 
 
         int n = dna.length;
-        if (n == 0) return false; // Vacío
+        if (n == 0) return false;
 
         for (String row : dna) {
-            if (row == null) return false; // Fila nula
-            if (row.length() != n) return false; // No es cuadrada (NxN)
-            if (!VALID_CHARACTERS.matcher(row).matches()) return false; // Caracteres inválidos
+            if (row == null) return false;
+            if (row.length() != n) return false;
+            if (!VALID_CHARACTERS.matcher(row).matches()) return false;
         }
 
         return true;
